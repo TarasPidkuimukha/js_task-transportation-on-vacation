d1ca374737678f1383fbf864a522927f4574ed1e
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2025-11-12 21:02:04 +0100

    solved1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,21 +5,21 @@
  */
 function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
-  const Price = days * COST_PER_DAY;
+  const price = days * COST_PER_DAY;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
 
   if (days >= LONG_TERM) {
-    return Price - LONG_TERM_DISCOUNT;
+    return price - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return Price - SHORT_TERM_DISCOUNT;
+    return price - SHORT_TERM_DISCOUNT;
   }
 
-  return Price;
+  return price;
 }
 
 module.exports = calculateRentalCost;
